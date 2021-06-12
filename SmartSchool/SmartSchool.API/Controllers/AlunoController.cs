622a8414e3f08f5143c69ae6bd6c121026a0d05b
Fernando Augusto <f3rnand0_lp@hotmail.com>
2021-06-12 16:04:05 -0400

    Sobe

---
+++ SmartSchool/SmartSchool.API/Controllers/AlunoController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SmartSchool.API.Models
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+
+        public List<Aluno> Alunos = new List<Aluno>(){
+            new Aluno(){
+                Id = 1,
+                Nome = "Fernando Augusto",
+                Sobrenome = "da Silva Gallardo",
+                Telefone = "123454356"
+            },
+            new Aluno(){
+                Id = 2,
+                Nome = "Alana Augusto",
+                Sobrenome = "Silva Silva",
+                Telefone = "345345345"
+            },
+            new Aluno(){
+                Id = 3,
+                Nome = "Morgana Augusto",
+                Sobrenome = "ldKSO DOAKS",
+                Telefone = "56756756"
+            },
+        };
+
+        // GET: api/<AlunoController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Alunos);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var aluno = Alunos.FirstOrDefault(a => a.Id == id);
+            if (aluno == null)
+                return BadRequest("O Aluno não foi encontrado");
+            else
+                return Ok(aluno);
+        }
+    }
+}
---
+++ SmartSchool/SmartSchool.API/Models/Disciplina.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace SmartSchool.API.Models
+{
+  public class Disciplina
+  {
+    public Disciplina() { }
+
+    public Disciplina(int id, string nome, int propessorId)
+    {
+      this.Id = id;
+      this.Nome = nome;
+      this.PropessorId = propessorId;
+    }
+
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public int PropessorId { get; set; }
+    public Professor Professor { get; set; }
+    public IEnumerable<AlunoDisciplina> AlunosDisciplinas { get; set; }
+  }
+}
