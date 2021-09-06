9e2de71b5c7bfb3599e8564888bccca3492eda06
Fernando Augusto <f3rnand0_lp@hotmail.com>
2021-09-06 14:56:32 -0400

    Nav e Titulo

--- SmartSchool/SmartSchoolApp/src/app/app.module.ts
+++ SmartSchool/SmartSchoolApp/src/app/app.module.ts
@@ -6,15 +6,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { AlunosComponent } from './components/alunos/alunos.component';
+import { ProfessoresComponent } from './components/professores/professores.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { TitleComponent } from './components/shared/title/title.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlunosComponent,
+    ProfessoresComponent,
+    PerfilComponent,
+    NavComponent,
+    TitleComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
     NgxSpinnerModule,
     ToastrModule.forRoot({
       timeOut: 3500,
@@ -22,7 +36,8 @@ import { NgxSpinnerModule } from 'ngx-spinner';
       preventDuplicates: true,
       progressBar: true,
       closeButton: true
-    })
+    }),
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
